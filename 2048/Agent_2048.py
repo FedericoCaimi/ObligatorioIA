3b039916c2663fa9d5c3c2843bbdc2c3eda120e5
Joaquin Mendez <fliamendez.98@gmail.com>
2020-06-24 13:53:53 -0300

    Arregle board_smoothness con los comentarios del profe

--- 2048/Agent_2048.py
+++ 2048/Agent_2048.py
@@ -16,7 +16,7 @@ class Agent2048(Agent):
 
     def play(self, board: GameBoard):
         #Debe retornar un movimiento
-        action,_ = self.expectimax(board, 3, 1)
+        action,_ = self.minimax(board, 4, 1)
         return action
 
     def heuristic_utility(self, board: GameBoard):
@@ -35,34 +35,30 @@ class Agent2048(Agent):
                 - Obtener la cantidad de celdas vacias
                 - Multiplicar por un empty_weight (recomendable en el orden de las decenas de miles)
         """
-        return self.board_value(board)
+        return self.board_smoothness(board)
 
-    def board_smoothness(self, board, weight: float = 1):
+    def board_smoothness(self, board, weight: float = 0.1):
         for x in range(4):
             for y in range(4):
                 board.grid[x][y] = board.grid[x][y] ** 0.5
         count = 0
         for x in range(4):
             for y in range(3):
-                #DIFERENCIA EN VALOR ABSOLUTO?
-                count += board.grid[x][y] - board.grid[x][y + 1]
+                count += abs(board.grid[x][y] - board.grid[x][y + 1])
         for x in range(3):
             for y in range(4):
-                #DIFERENCIA EN VALOR ABSOLUTO?
-                count += board.grid[x][y] - board.grid[x + 1][y]
+                count += abs(board.grid[x][y] - board.grid[x + 1][y])
         return count * weight * -1
 
 
     def board_value(self, board):
-        #PREGUNTAR SI HAY QUE ELEVAR CADA CASILLA AL CUADRADO, O QUE SE REFIERE?
         count = 0
         for x in range(4):
             for y in range(4):
                 count += board.grid[x][y] ** 2
         return count
     
-    def board_blank_spaces(self, board, weight:float = 0.000001):
-        #PREGUNTAR QUE BUEN VALOR PODRIA TENER EL WEIGHT
+    def board_blank_spaces(self, board, weight:float = 10000):
         return len(board.get_available_cells()) * weight
 
     #EL AMBIENTE 2048 SE PUEDE MODELAR COMO UN AGENTE QUE TIRA FICHAS DE MANERA RANDOM
